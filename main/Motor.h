b8d73e6270ce0c805eeff33ce68140b4ea73a40d
Ryosuke Sasaki <rsasaki@keio.jp>
2022-03-04 10:08:37 +0900

    燃える

---
+++ main/Motor.h
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#include "PinAssign.h"
+
+class Motor
+{
+    public:
+    /**
+     * @brief モーター初期化
+     * @param None
+     */
+    void Init() {
+        pinMode(MotorA_LineA, OUTPUT);
+        pinMode(MotorA_LineB, OUTPUT);
+        pinMode(MotorB_LineA, OUTPUT_OPEN_DRAIN);
+        pinMode(MotorB_LineB, OUTPUT_OPEN_DRAIN);
+        //初期状態は停止
+        MotorA_Rotate(0);
+        MotorB_Rotate(0);
+    }
+
+    /**
+     * @brief モーターAの回転制御
+     * @param int dir 
+     * dir = 1      : 正転
+     * dir = -1     : 逆転
+     * otherwise    : 停止
+     */
+    void MotorA_Rotate(int dir=0) {
+        if (dir == 1) {
+            digitalWrite(MotorA_LineA, LOW);
+            digitalWrite(MotorA_LineB, HIGH);
+        } else if (dir == -1) {
+            digitalWrite(MotorA_LineA, HIGH);
+            digitalWrite(MotorA_LineB, LOW);
+        } else {
+            digitalWrite(MotorA_LineA, LOW);
+            digitalWrite(MotorA_LineB, LOW);
+        }
+    }
+
+    /**
+     * @brief モーターBの回転制御
+     * @param int dir 
+     * dir = 1      : 正転
+     * dir = -1     : 逆転
+     * otherwise    : 停止
+     */
+    void MotorB_Rotate(int dir=0) {
+        if (dir == 1) {
+            digitalWrite(MotorB_LineA, LOW);
+            digitalWrite(MotorB_LineB, HIGH);
+        } else if (dir == -1) {
+            digitalWrite(MotorB_LineA, HIGH);
+            digitalWrite(MotorB_LineB, LOW);
+        } else {
+            digitalWrite(MotorB_LineA, LOW);
+            digitalWrite(MotorB_LineB, LOW);
+        }
+    }
+
+};
--- main/main.ino
+++ main/main.ino
@@ -3,23 +3,29 @@
 #include "Dps310.h"
 #include "BMX055.h"
 #include "MadgwickAHRS.h"
+#include "Motor.h"
 
 Madgwick madgwick;
 Dps310 PressureSensor = Dps310();
 BMX055 nineDoFSensor = BMX055();
+Motor motor = Motor();
 int16_t ret;
 
 void setup() {
     Wire.begin(I2C_SDA, I2C_SCL);
     Serial.begin(115200);
+    pinMode(19, OUTPUT);
+    digitalWrite(19, LOW);
     /**
     PressureSensor.begin(Wire);
     ret = PressureSensor.startMeasureBothCont(4,2,4,2);
     **/
-
+    /** 
     madgwick.begin(10); //1Hz (1000msec周期)
     nineDoFSensor.BMX055_Init(Wire);
     delay(100);
+    **/
+    motor.Init();
 }
 
 void loop() {
@@ -30,6 +36,7 @@ void loop() {
         Serial.println(heightMetre);
     }
     **/
+    /**
     madgwick.update(nineDoFSensor.xGyro, nineDoFSensor.yGyro, nineDoFSensor.zGyro,
     nineDoFSensor.xAccl, nineDoFSensor.yAccl, nineDoFSensor.zAccl, 
     nineDoFSensor.xMag, nineDoFSensor.yMag, nineDoFSensor.zMag);
@@ -49,33 +56,8 @@ void loop() {
     Serial.print("yaw= ");
     Serial.print(yaw);
     Serial.println(""); 
-    /**
-    //BMX055 加速度の読み取り
-    Serial.print("Accl= ");
-    Serial.print(nineDoFSensor.xAccl);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.yAccl);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.zAccl);
-    Serial.println(""); 
-    //BMX055 ジャイロの読み取り
-    Serial.print("Gyro= ");
-    Serial.print(nineDoFSensor.xGyro);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.yGyro);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.zGyro);
-    Serial.println(""); 
-    //BMX055 磁気の読み取り
-    Serial.print("Mag= ");
-    Serial.print(nineDoFSensor.xMag);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.yMag);
-    Serial.print(",");
-    Serial.print(nineDoFSensor.zMag);
-    Serial.println(""); 
-    Serial.print(cos(nineDoFSensor.xMag*3.14/1024));
-    Serial.println("");
     **/
+    motor.MotorA_Rotate(-1);
+    motor.MotorB_Rotate(0);
     delay(100);
 }

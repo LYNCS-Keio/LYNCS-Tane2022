c368322aa030a871847dbdbb9bfd1b4ee5563a7d
Ryosuke Sasaki <rsasaki@keio.jp>
2022-03-04 21:35:56 +0900

    GNSSの動作確認

---
+++ main/GNSS.h
@@ -0,0 +1,51 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+class GNSS
+{
+    public:
+        void Init(SoftwareSerial &bus, TinyGPSPlus &gps)
+        {
+            _bus = &bus;
+            _gps = &gps;
+        }
+        int get_azimuth(double &azimuth)
+        {
+            int ret = 0;
+            while (_bus->available() > 0){
+                char c = _bus->read();
+                //Serial.print(c);
+                _gps->encode(c);
+                if (_gps->location.isUpdated()){
+                    now_longitude = _gps->location.lng();
+                    now_latitude = _gps->location.lat();
+                    now_altitude = _gps->altitude.meters();
+                    azimuth = azimuth_cal(now_longitude,now_latitude);
+                    ret = 1;
+                }
+            }
+            return ret;
+        }
+        
+    private:
+        SoftwareSerial *_bus;
+        TinyGPSPlus *_gps;
+        double const pi = 3.14159265358;
+        double const radius = 6378.137;
+        double goal_longitude = 139.65670;
+        double goal_latitude = 35.56784;
+        double now_latitude;
+        double now_longitude;
+        double now_altitude;
+
+        double azimuth_cal(double now_longitude,double now_latitude){
+            double nlng_rad = now_longitude*pi/180;
+            double nlat_rad = now_latitude*pi/180; 
+            double glng_rad = goal_longitude*pi/180;
+            double glat_rad = goal_latitude*pi/180;
+
+            double temp_d = radius*acos(sin(nlat_rad)*sin(glat_rad) + cos(nlat_rad)*cos(glat_rad)*cos(glng_rad - nlng_rad));
+            double azimuth = pi + atan2(sin(glng_rad - nlng_rad),cos(nlat_rad)*tan(glat_rad) - sin(nlat_rad)*cos(glng_rad - nlng_rad));
+            return azimuth * 180 / pi;
+        }
+};
--- main/main.ino
+++ main/main.ino
@@ -1,36 +1,51 @@
 #include <Wire.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 #include "PinAssign.h"
 #include "Dps310.h"
 #include "BMX055.h"
 #include "MadgwickAHRS.h"
 #include "Motor.h"
+#include "GNSS.h"
 
+TinyGPSPlus tinygps;
+SoftwareSerial sSerial(GNSS_RX, GNSS_TX);
 Madgwick madgwick;
 Dps310 PressureSensor = Dps310();
 BMX055 nineDoFSensor = BMX055();
 Motor motor = Motor();
+GNSS gps = GNSS();
 int16_t ret;
+double target_azimuth;
 
 void setup() {
     Wire.begin(I2C_SDA, I2C_SCL);
     Serial.begin(115200);
+    Serial.println(F("LYNCS Tane 2022"));
+    sSerial.begin(9600);
     pinMode(19, OUTPUT);
     digitalWrite(19, LOW);
+    motor.Init();
     /**
     PressureSensor.begin(Wire);
     ret = PressureSensor.startMeasureBothCont(4,2,4,2);
     **/
-    /** 
+    /**/
     madgwick.begin(10); //1Hz (1000msec周期)
     nineDoFSensor.BMX055_Init(Wire);
     delay(100);
-    **/
-    motor.Init();
+    /**/
+    gps.Init(sSerial, tinygps);
 }
 
 void loop() {
+    Serial.println("--------------------------------------"); 
+    if (gps.get_azimuth(target_azimuth)) {
+        Serial.print("target azimuth = ");
+        Serial.println(target_azimuth);
+    }
     /**
-     float heightMetre;
+    float heightMetre;
     ret = PressureSensor.calcHeighMetre(heightMetre, 100820);
     if (ret == 0) {
         Serial.println(heightMetre);
@@ -46,21 +61,13 @@ void loop() {
     nineDoFSensor.BMX055_Accl();
     nineDoFSensor.BMX055_Gyro();
     nineDoFSensor.BMX055_Mag();
-    Serial.println("--------------------------------------"); 
-    Serial.print("roll= ");
-    Serial.print(roll);
-    Serial.println("");
-    Serial.print("pitch= ");
-    Serial.print(pitch);
-    Serial.println(""); 
     Serial.print("yaw= ");
     Serial.print(yaw);
     Serial.println(""); 
     **/
+    /**
     motor.MotorA_Rotate(-1);
     motor.MotorB_Rotate(1);
-    delay(2000);
-    motor.MotorA_Rotate(1);
-    motor.MotorB_Rotate(-1);
-    delay(2000);
+    **/
+    delay(100);
 }
